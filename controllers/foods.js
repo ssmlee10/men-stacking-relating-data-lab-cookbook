af0d2ce812b987fe1e37420a99b941b5d6fed30e
Sharon Lee <ssmlee10@gmail.com>
2025-07-25 07:28:13 -0700

    create navbar partial, define the index route for foods

--- controllers/foods.js
+++ controllers/foods.js
@@ -4,5 +4,9 @@ const router = express.Router();
 const User = require('../models/user.js');
 
 // router lgoic will go here
+router.get('/', (req, res) => {
+    res.render('foods/index.ejs');
+});
+
 
 module.exports = router;
