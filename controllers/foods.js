c0ffa411c94fa66996fb328d8bbbf100b67bd4e7
Sharon Lee <ssmlee10@gmail.com>
2025-07-26 00:26:07 -0700

    full functionality of show.ejs page for all foods

--- controllers/foods.js
+++ controllers/foods.js
@@ -3,13 +3,11 @@ const router = express.Router();
 
 const User = require('../models/user.js');
 
-// router logic will go here
-// routing to the pantry foods index page
+// GET to the pantry index page
 router.get('/', async (req, res) => {
   try {
     // look up user from req.session
     const currentUser = await User.findById(req.session.user._id);
-    console.log('pantry:', currentUser.pantry);
     // render index.ejs, passing in all of the current user's foods as data in the context object
     res.render('foods/index.ejs', {
       pantry: currentUser.pantry
@@ -20,7 +18,12 @@ router.get('/', async (req, res) => {
   }
 });
 
-// creating a new food
+// GET to the new foods form page
+router.get('/new', (req, res) => {
+  res.render('foods/new.ejs');
+});
+
+// POST a new food to the db
 router.post('/', async (req, res) => {
   try {
     const currentUser = await User.findById(req.session.user._id);
@@ -28,6 +31,23 @@ router.post('/', async (req, res) => {
     await currentUser.save();
     res.redirect(`/users/${currentUser._id}/foods`);
   } catch (error) {
+    console.log(error);
+    res.redirect('/');
+  }
+});
+
+// GET to the individual food pages
+router.get('/:foodId', async (req, res) => {
+  try {
+    // look up the user from req.session
+    const currentUser = await User.findById(req.session.user._id);
+    // find the food by the foodID supplied from req.params
+    const food = currentUser.pantry.id(req.params.foodId);
+    // render the show view, passing the food data into the context object
+    res.render('foods/show.ejs', {
+      food: food,
+    });
+  } catch(error) {
     console.log(error);
     res.redirect('/');
   }
